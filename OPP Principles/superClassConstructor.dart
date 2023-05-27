55b4399c4808aaaa1c64457eeb698626391c652f
usmansiddiqui12321 <usmansiddiqui12321@gmail.com>
2023-05-27 18:13:00 +0500

    commit

--- OPP Principles/superClassConstructor.dart
+++ OPP Principles/superClassConstructor.dart
@@ -8,11 +8,6 @@ class Person {
 
 class Student extends Person {
   int? fee;
-  @override
-  greet() {
-    // TODO: implement greet
-    return super.greet();
-  }
 }
 
 class Teacher extends Student {
@@ -25,8 +20,8 @@ void main(List<String> args) {
   student.fee = 10000;
   student.greet();
   Teacher teacher_one = Teacher();
-teacher_one.id = "2" ; 
-teacher_one.salary=50000;
-teacher_one.name = "aqib";
-teacher_one.greet();
+  teacher_one.id = "2";
+  teacher_one.salary = 50000;
+  teacher_one.name = "aqib";
+  teacher_one.greet();
 }
