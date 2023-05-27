4ac39ccc5872f4d35f4919516a693774b6441f5f
usmansiddiqui12321 <usmansiddiqui12321@gmail.com>
2023-05-27 18:13:24 +0500

    commit

---
+++ OPP Principles/2.superClassConstructor.dart
@@ -0,0 +1,37 @@
+class Person {
+  String? id;
+  String? name;
+  Person({required this.name, required this.id});
+  //Yahan Required 2 chezen hn name or id 
+  //tw jo jo isko extend kry ga 
+  //usy Super lga kr isi ko extend krna pryga 
+  
+  greet() {
+    print('Hello $name');
+  }
+}
+
+class Student extends Person {
+  Student({required name, required id, required this.fee})
+      : super(id: id, name: name);
+
+  int fee;
+}
+
+class Teacher extends Student {
+  Teacher({required name, required id, required this.salary, required fee})
+      : super(id: id, name: name, fee: fee);
+
+  int salary;
+}
+
+void main(List<String> args) {
+  Student student =
+      Student(id: "4", name: "Muhammad Usman Siddiqui", fee: 1000);
+  student.fee = 10000;
+  student.greet();
+  Teacher teacher_one =
+      Teacher(id: "5", name: "Aqib", fee: 5000, salary: 10000);
+  teacher_one.salary = 50000;
+  teacher_one.greet();
+}
---
+++ OPP Principles/3.method_Override.dart
@@ -0,0 +1,43 @@
+class Person {
+  String? id;
+  String? name;
+  Person({required this.name, required this.id});
+  //Main Goal is to Make Greet Function for Every class
+  greet() {
+    print('Hello $name');
+  }
+}
+
+class Student extends Person {
+  Student({required name, required id, required this.fee})
+      : super(id: id, name: name);
+
+  int fee;
+@override
+ greet() {
+    print('Hello $name , your Student id is $id and Your Fee is $fee');
+  }
+}
+
+class Teacher extends Student {
+  Teacher({required name, required id, required this.salary, required fee})
+      : super(id: id, name: name, fee: fee);
+
+  int salary;
+@override
+
+greet() {
+    print('Hello $name , your Student id is $id and Your salary is $salary');
+  }
+}
+
+void main(List<String> args) {
+Person person = Person(name: "ALi", id: "500");
+person.greet();
+  Student student =
+      Student(id: "4", name: "Muhammad Usman Siddiqui", fee: 1000);
+  student.greet();
+  Teacher teacher_one =
+      Teacher(id: "5", name: "Aqib", fee: 5000, salary: 10000);
+  teacher_one.greet();
+}
